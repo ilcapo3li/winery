42a7a43d2fa7211f6a59219b8916fedec5ee33eb
ilcapo <38299644+ilcapo3li@users.noreply.github.com>
2020-03-28 09:34:21 +0300

    fix redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Wine;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('wine.create');
+        $wine = Wine::all();
+
+        return view('wine.create',compact('wine'));
     }
 }
